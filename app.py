d8fa22feb8a15ff272921ca96f86168dd3feeaf0
Sumit Sen <116353440+sumit96367@users.noreply.github.com>
2025-07-16 11:47:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def hello_world():
+    return 'Hello Everyone!'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/health')
+def health():
+    return 'Server is up and running'
